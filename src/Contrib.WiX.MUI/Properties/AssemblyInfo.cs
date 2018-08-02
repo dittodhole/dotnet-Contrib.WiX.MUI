da093921d4f65a524f59fa94000b7ff9227e4244
Andreas Niedermair <dev@andreas.niedermair.name>
2018-08-02 15:33:56 +0200

    init of code
    work in progress :beers:

---
+++ src/Contrib.WiX.MUI/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using global::System.Reflection;
+using global::System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("Contrib.WiX.MUI")]
+[assembly: AssemblyDescription("Create multi-language Windows Installer with WiX Toolset ")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Andreas Niedermair (@dittodhole)")]
+[assembly: AssemblyProduct("Contrib.WiX.MUI")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("84658b7b-fa73-4980-a2ed-fdace7dcf4f8")]
