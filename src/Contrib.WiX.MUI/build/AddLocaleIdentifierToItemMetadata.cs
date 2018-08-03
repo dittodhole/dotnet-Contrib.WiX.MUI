42396abae54db6c76608a093309afa5aafb8cda7
Andreas Niedermair <dev@andreas.niedermair.name>
2018-08-03 12:07:44 +0200

    feat: adding localeIdentifier to item metadata

---
+++ src/Contrib.WiX.MUI/build/AddLocaleIdentifierToItemMetadata.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+// ReSharper disable CheckNamespace
+// ReSharper disable MemberCanBeProtected.Global
+// ReSharper disable MemberCanBePrivate.Global
+
+namespace Contrib.WiX.MUI
+{
+  public partial class AddLocaleIdentifierToItemMetadata : Task
+  {
+    public const string CultureMetadataName = "Culture";
+    public const string LocaleIdentifierMetadataItemName = "LocaleIdentifier";
+
+    [Required]
+    public virtual ITaskItem[] Items { get; set; }
+
+    /// <inheritdoc />
+    public override bool Execute()
+    {
+      var result = true;
+      foreach (var item in this.Items)
+      {
+        int localeIdentifier;
+
+        var culture = item.GetMetadata(AddLocaleIdentifierToItemMetadata.CultureMetadataName);
+        if (string.IsNullOrEmpty(culture))
+        {
+          localeIdentifier = 0;
+        }
+        else if (string.Equals(culture,
+                               "neutral",
+                               StringComparison.OrdinalIgnoreCase))
+        {
+          localeIdentifier = 0;
+        }
+        else
+        {
+          try
+          {
+            var cultureInfo = CultureInfo.GetCultureInfo(culture);
+            localeIdentifier = cultureInfo.LCID;
+          }
+          catch (CultureNotFoundException)
+          {
+            localeIdentifier = 0;
+            result = false;
+          }
+        }
+
+        item.SetMetadata(AddLocaleIdentifierToItemMetadata.LocaleIdentifierMetadataItemName,
+                         localeIdentifier.ToString());
+      }
+      return result;
+    }
+  }
+}
