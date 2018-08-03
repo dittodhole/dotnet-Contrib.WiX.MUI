edee384ed6f6c0650726dc5125dcaa6c7f8945a5
Andreas Niedermair <dev@andreas.niedermair.name>
2018-08-03 10:55:42 +0200

    feat: added GetWilangidPath

---
+++ src/Contrib.WiX.MUI/build/GetWilangidPath.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+// ReSharper disable CheckNamespace
+// ReSharper disable MemberCanBeProtected.Global
+// ReSharper disable MemberCanBePrivate.Global
+
+namespace Contrib.WiX.MUI
+{
+  public partial class GetWilangidPath : Task
+  {
+    public const string WilangidFileName = "wilangid.vbs";
+
+    [Output]
+    public virtual string Path { get; set; }
+
+    /// <inheritdoc />
+    public override bool Execute()
+    {
+      this.Path = ToolLocationHelper.GetPathToWindowsSdkFile(GetWilangidPath.WilangidFileName,
+                                                             TargetDotNetFrameworkVersion.VersionLatest,
+                                                             VisualStudioVersion.VersionLatest,
+                                                             DotNetFrameworkArchitecture.Current);
+
+      var result = !string.IsNullOrEmpty(this.Path);
+
+      return result;
+    }
+  }
+}
