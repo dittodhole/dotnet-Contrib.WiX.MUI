38f89b6dc47bf6ba3cfaecfaa07c3f9bb33b15c9
Andreas Niedermair <dev@andreas.niedermair.name>
2018-08-03 10:32:37 +0200

    feat: added GetWibsubstgPath

---
+++ src/Contrib.WiX.MUI/build/GetWisubstgPath.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+// ReSharper disable CheckNamespace
+// ReSharper disable MemberCanBeProtected.Global
+// ReSharper disable MemberCanBePrivate.Global
+
+namespace Contrib.WiX.MUI
+{
+  public partial class GetWisubstgPath : Task
+  {
+    public const string WisubstgFileName = "wisubstg.vbs";
+
+    [Output]
+    public virtual string Path { get; set; }
+
+    /// <inheritdoc />
+    public override bool Execute()
+    {
+      this.Path = ToolLocationHelper.GetPathToWindowsSdkFile(GetWisubstgPath.WisubstgFileName,
+                                                             TargetDotNetFrameworkVersion.VersionLatest,
+                                                             VisualStudioVersion.VersionLatest,
+                                                             DotNetFrameworkArchitecture.Current);
+
+      var result = !string.IsNullOrEmpty(this.Path);
+
+      return result;
+    }
+  }
+}
